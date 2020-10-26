612532dc798359ddea4aa7f61625c64e3110dc42
MMcClure11 <mcclme02@luther.edu>
2020-10-26 09:25:55 -0500

    adds stephen's first and second solves

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,12 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//My solve 15 min first time
+
+// function reverse(str) {
+//   let arr = str.split('')
+//   // console.log(arr)
+//   let newArr = arr.reverse()
+//   // console.log(newArr.join(''))
+//   return newArr.join('') 
+// }
+
+// Stephen Solve 1 "easiest"
+
+// function reverse(str) {
+//   return str.split('').reverse().join('') 
+// }
+
+// Stephen Solve 2, "Don't use reverse Helper"
+
 function reverse(str) {
-  let arr = str.split('')
-  // console.log(arr)
-  let newArr = arr.reverse()
-  // console.log(newArr.join(''))
-  return newArr.join('') 
+   reversed = '';
+
+   for (let character of str){
+     reversed = character + reversed;
+   }
+   
+   return reversed;
+}
+
+// Stephen Solve 3
+
+function reverse(str){
+
 }
 
 module.exports = reverse;
