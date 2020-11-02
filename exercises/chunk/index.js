4cae750ce1e5232d236ba861c98d7253f1e815c3
MMcClure11 <mcclme02@luther.edu>
2020-11-02 09:11:55 -0600

    solve 1 for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//my first thoughts...
+// function chunk(array, size) {
+//   let newArr = []
+//   newArr.push(array.splice(0, size))
+//   console.log(newArr)
+//   console.log(array)
+//   console.log(array.splice(size - 2, size))
+// }
+
+//Stephen's Solve 1
+function chunk(array, size) {
+  const chunked = [];
+  for (let element of array) {
+    const last = chunked[chunked.length - 1]
+    if (!last || last.length === size) {
+      chunked.push([element])
+    } else {
+      last.push(element)
+    }
+  }
+  return chunked
+}
 
 module.exports = chunk;
