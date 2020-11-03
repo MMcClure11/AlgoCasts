2454693fc69976b9d8b59bc055b61490da85a42e
MMcClure11 <mcclme02@luther.edu>
2020-11-03 09:22:27 -0600

    adds three solves for anagrams including mine and two of Stephen's

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,83 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//Stephen's Solve 2. has some interesting performance issues we'll discuss in the future
+// uses array method: .sort()
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+//remove spaces and puncuation
+//lower case the string
+//RegExp to remove spaces and punctuation /w
+// const word = "Hi ThERE!!!"
+// let newWord = word.replace(/[^\w]/g, "").toLowerCase();
+// console.log(newWord)
 
 module.exports = anagrams;
+
+//my first attempt...i did it!
+// function anagrams(stringA, stringB) {
+//   const objA = {}
+//   const objB = {}
+//   const strA = stringA.replace(/[^\w]/g, "").toLowerCase()
+//    const strB = stringB.replace(/[^\w]/g, "").toLowerCase()
+//   for (let char of strA ) {
+//     if (!objA[char]) {
+//       objA[char] = 1
+//     } else {
+//       objA[char] += 1
+//     }
+//   }
+//   for (let char of strB ) {
+//     if (!objB[char]) {
+//       objB[char] = 1
+//     } else {
+//       objB[char] += 1
+//     }
+//   }
+  
+//   if (Object.keys(objA).length !== Object.keys(objB).length) {
+  //   return false
+  // }
+  // for (let k in objA) {
+  //   if (objA[k] === objB[k] ) {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
+// }
+
+//Stephen's Solve 1, make a helper function for making the charMap!
+//notice when making this solve we iterate through stringA once to build aCharMap, once for stringB and again for comparing the two charMaps
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+  
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char] ) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//   return charMap
+// }
