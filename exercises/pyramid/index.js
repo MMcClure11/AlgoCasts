0da43d9bb096a339150e923e6376229de9ae31f4
MMcClure11 <mcclme02@luther.edu>
2020-11-06 08:28:30 -0600

    adds iterative solution for pyramids

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//Stephen solve one. I got how to get number of columns, missed how to get determine if it should have a #
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n -1) / 2);
+
+  for (let row = 0; row < n; row ++) {
+    let level = '';
+    for (let column = 0; column < (2 * n - 1); column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level)
+  }
+}
+
+// Math.floor(5.235) //=> 5
 
 module.exports = pyramid;
