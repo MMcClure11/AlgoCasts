8b8c5db80bcd235497adf039e9c1700b0f65a457
MMcClure11 <mcclme02@luther.edu>
2020-11-09 09:06:27 -0600

    adds stephen's iterative and regex solutions for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,9 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-//Stephen's Solve 1 - iterative
+//Stephen's Solve 2 - regex
+//.match looks for anything that matches inside the [], 
+//the g is to make it go through all the string, not stop at the first match,
+//the i makes it case insensitive
+//matches returns either an array or null
 function vowels(str) {
-  
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0
 }
 
 module.exports = vowels;
@@ -17,16 +23,31 @@ module.exports = vowels;
 
 //my own first attempt, passes 15 minutes, forgot how to build up the object (obj[vowels] = obj[vowels] + 1 || 1)
 // function vowels(str) {
-//   let obj = {}
-//   for (let char of str) {
-//     if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
-//     char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
-//       obj[vowels] = obj[vowels] + 1 || 1
+  // let obj = {}
+  // for (let char of str) {
+  //   if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+  //   char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+  //     obj[vowels] = obj[vowels] + 1 || 1
+  //   }
+  // }
+  // if (obj[vowels]) {
+  //   return obj[vowels]
+  // } else {
+  //   return 0
+  // }
+// }
+
+//Stephen's Solve 1 - iterative; use array for checker to make it easier in future to add or remove letters, 
+//by using built in .includes can clean up really messy code from up above
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
 //     }
 //   }
-//   if (obj[vowels]) {
-//     return obj[vowels]
-//   } else {
-//     return 0
-//   }
+
+//   return count;
 // }
