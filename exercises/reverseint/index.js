291abc0ec95fd76cc08fda6fd7dc4e08e47b8391
MMcClure11 <mcclme02@luther.edu>
2021-01-15 10:07:45 -0600

    Add solve for reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,20 +10,20 @@
 
 //my 1st attempt, works but very clunky jaja
 
-function reverseInt(n) {
-  const numArr = n.toString().split('')
-  let newArr = []
-  if (Math.sign(n) === -1){
-    newArr.push("-")
-    let alteredReverseArr = numArr.slice(1).reverse()
-    let finalArr = newArr.concat(alteredReverseArr)
-    return parseInt(finalArr.join(''))
-  } else {
-    let reverseArr = numArr.reverse()
-    let finalArr = newArr.concat(reverseArr)
-    return parseInt(finalArr.join(''))
-  }
-}
+// function reverseInt(n) {
+//   const numArr = n.toString().split('')
+//   let newArr = []
+//   if (Math.sign(n) === -1){
+//     newArr.push("-")
+//     let alteredReverseArr = numArr.slice(1).reverse()
+//     let finalArr = newArr.concat(alteredReverseArr)
+//     return parseInt(finalArr.join(''))
+//   } else {
+//     let reverseArr = numArr.reverse()
+//     let finalArr = newArr.concat(reverseArr)
+//     return parseInt(finalArr.join(''))
+//   }
+// }
 
 // Stephen's Solve
 // function reverseInt(n){
@@ -35,5 +35,11 @@ function reverseInt(n) {
 
   // return parseInt(reversed) * Math.sign(n);
 // }
+
+//1/15/21 solve
+function reverseInt(num) {
+  let reversed = num.toString().split('').reverse().join('')
+  return parseInt(reversed) * Math.sign(num)
+}
 
 module.exports = reverseInt;
