d0cea88f7f9c934adb0c54a6c2e05dc5bde350ed
MMcClure11 <mcclme02@luther.edu>
2021-01-25 15:01:30 -0600

    Add solve for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,29 +17,45 @@
 //       '### '
 //       '####'
 
+
+
+function steps(n) {
+  for(let r = 1; r <= n; r++){
+    let stair = ''
+    for(let c = 1; c <= n; c++){
+      if(c <= r){
+        stair += '#'
+      } else {
+        stair += ' '
+      }
+    }
+    console.log(stair)
+  }
+}
+
+module.exports = steps;
+
 //Stephen Solve two, recursion solution
 // not recommended for interview setting because it's tricky
 // unless they specifically ask for recursion
 
-function steps(n, row = 0, stair = '') {
-  if (n === row) {
-    return;
-  }
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
 
-  if (n === stair.length) {
-    console.log(stair);
-    return steps(n, row + 1);
-  }
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
 
-  if (stair.length <= row) {
-    stair += '#';
-  } else {
-    stair += ' ';
-  }
-  steps(n, row, stair);
-}
-
-module.exports = steps;
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   steps(n, row, stair);
+// }
 
 
 //Stephen Solve one, best option for an interview setting
